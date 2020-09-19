c9eadff073bc1a90d24bf2c0fe7e8e3b1e4245a4
xx-shade <68170263+xx-shade@users.noreply.github.com>
2020-09-19 19:43:21 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var side1, side2, side3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:.04, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	 side1 = Bodies.rectangle(350, 620, 20, 100,{isStatic : true });
+	 side2 = Bodies.rectangle(450, 650, 200, 20,{isStatic : true });
+	 side3 = Bodies.rectangle(550, 620, 20, 100,{isStatic : true });
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+
+  rectMode(CENTER);
+  fill("red");
+  rect(side1.position.x, side1.position.y,  20,100);
+  rect(side2.position.x, side2.position.y,  200,20);
+  rect(side3.position.x, side3.position.y,  20,100);
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody, false);
+	
+  }
+}
+
+
+
